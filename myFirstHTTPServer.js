20caa7238a5fb79f5c46dbabe3829b3d3be0cf21
Jasdeep Kahlon <jasdeep.kln@gmail.com>
2016-12-17 23:08:34 -0800

    set up jas.cat

---
+++ myFirstHTTPServer.js
@@ -0,0 +1,42 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/index.html", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('/index.html');
+});    
+
+//Lets use our dispatcher
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+// //A sample POST request
+// dispatcher.onPost("/post1", function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Got Post Data');
+// });
